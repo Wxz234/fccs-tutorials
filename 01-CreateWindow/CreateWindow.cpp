2452be337008ecb28a4c7c947bce7ed5daa6c3f5
Wxz <78699242+Wxz234@users.noreply.github.com>
2021-11-01 20:54:57 +0800

    1

---
+++ 01-CreateWindow/CreateWindow.cpp
@@ -0,0 +1,20 @@
+//#include <FCCS.h>
+//using namespace FCCS;
+//
+#include <Windows.h>
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+//	GameWindow window(L"fccs_game", 800, 600);
+//	bool is_run = true;
+//	while (is_run) {
+//		Message message;
+//		if (PollMessage(message)) {
+//			switch (message.type)
+//			{
+//			case MESSAGE_TYPE::FCCS_QUIT:
+//				is_run = false;
+//				break;
+//			}
+//		}
+//	}
+//	return 0;
+}
