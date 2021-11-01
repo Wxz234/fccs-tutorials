8b8168222bcc032a744d51c7d3a6c606bd0bb731
Wxz <78699242+Wxz234@users.noreply.github.com>
2021-11-01 23:14:26 +0800

    finish tutorial2

--- 02-InitDevice/InitDevice.cpp
+++ 02-InitDevice/InitDevice.cpp
@@ -4,10 +4,17 @@ using namespace FCCS;
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	RefCountPtr<IWindow> window = CreateWindowExW(L"fccs", 800, 600);
 	RefCountPtr<Graphics::IDevice> device = Graphics::CreateDevice();
-	//RefCountPtr<Graphics::ISwapChain> device = Graphics::CreateSwapChain()
+	RefCountPtr<Graphics::ICommandQueue> comandqueue = device->CreateCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
+	Graphics::FCCS_SWAP_CHAIN_DESC desc{};
+	desc.BufferCount = 3;
+	desc.Width = 800;
+	desc.Height = 600;
+	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	RefCountPtr<Graphics::ISwapChain> swapchain = Graphics::CreateSwapChain(comandqueue.Get(), window.Get(), &desc);
+	
 	window->ShowWindow();
 	while (window->IsActive()) {
-
+		swapchain->Present();
 	}
 	return 0;
 }
