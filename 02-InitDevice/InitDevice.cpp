b4da1c044331a5adcdd4846fd281c439825c417b
Wxz <78699242+Wxz234@users.noreply.github.com>
2021-11-01 22:31:41 +0800

    t2

---
+++ 02-InitDevice/InitDevice.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <FCCS.h>
+using namespace FCCS;
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	RefCountPtr<IWindow> window = CreateWindowExW(L"fccs", 800, 600);
+	RefCountPtr<Graphics::IDevice> device = Graphics::CreateDevice();
+	//RefCountPtr<Graphics::ISwapChain> device = Graphics::CreateSwapChain()
+	window->ShowWindow();
+	while (window->IsActive()) {
+
+	}
+	return 0;
+}
