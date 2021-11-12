dcb52fb319e8f1a7988c4b234d2ec8c07c78cdec
Wxz <429373960@qq.com>
2021-11-13 00:34:47 +0800

    update

--- 01-CreateWindow/CreateWindow.cpp
+++ 01-CreateWindow/CreateWindow.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
-
+#include <FCCS.h>
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	auto device = FCCS::RHI::CreateRHIDevice();
+
 	return 0;
 }
