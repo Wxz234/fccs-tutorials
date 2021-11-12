938a204474101383bce95b63d26b045b9370b766
Wxz <429373960@qq.com>
2021-11-12 23:30:45 +0800

    delete

--- 01-CreateWindow/CreateWindow.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-#include <FCCS.h>
-using namespace FCCS;
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-	RefCountPtr<IWindow> window;
-	CreateWindowExW(L"fccs", 800, 600, &window);
-	while (window->IsActive()) {}
-	return 0;
-}
