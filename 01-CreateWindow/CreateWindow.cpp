ad03c58155bddf6e167a3eda88c0130ce000ca17
Wxz <429373960@qq.com>
2021-11-18 22:50:55 +0800

    update t3

--- 01-CreateWindow/CreateWindow.cpp
+++ 01-CreateWindow/CreateWindow.cpp
@@ -1,6 +1,14 @@
 #include <Windows.h>
 #include <FCCS.h>
+
+class Context : public FCCS::WindowContext {
+public:
+	void Initialize() {}
+	void Update() {}
+	void Release() {}
+};
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	auto window = FCCS::CreateWindowExW(L"fccs", 800, 600);
-	return window->Run(nullptr);
+	Context context;
+	return window->Run(&context);
 }
