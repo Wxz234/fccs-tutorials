2226604733f6e568a24100809d25d155bd3d45bc
Wxz <429373960@qq.com>
2021-11-25 15:27:23 +0800

    Update InitDevice.cpp

--- 02-InitDevice/InitDevice.cpp
+++ 02-InitDevice/InitDevice.cpp
@@ -2,38 +2,33 @@
 #include <FCCS.h>
 class Context : public FCCS::WindowContext {
 public:
-	Context(const FCCS::RHI::FCCS_SWAP_CHAIN_DESC* pDesc) {
+	Context(const FCCS::FCCS_SWAP_CHAIN_DESC* pDesc) {
 		desc = *pDesc;
-		queue = nullptr;
 		device = nullptr;
 		swapchain = nullptr;
 	}
 
 	void Initialize() {
-		device = FCCS::RHI::CreateRHIDevice();
-		queue = device->CreateCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
-		swapchain = FCCS::RHI::CreateSwapChain(queue, &desc);
+		FCCS::RHI::CreateDeviceAndSwapChain(&desc, &device, &swapchain);
 	}
 	void Update() {
 		swapchain->Present();
 	}
 	void Release() {
 		FCCS::RHI::DestroyRHIObject(device);
-		FCCS::RHI::DestroyRHIObject(queue);
 		FCCS::RHI::DestroyRHIObject(swapchain);
 	}
 
 	FCCS::RHI::Device* device;
-	FCCS::RHI::CommandQueue* queue;
 	FCCS::RHI::SwapChain *swapchain;
-	FCCS::RHI::FCCS_SWAP_CHAIN_DESC desc;
+	FCCS::FCCS_SWAP_CHAIN_DESC desc;
 };
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	unsigned width = 800, height = 600;
 	constexpr unsigned bufferCount = 3;
 	auto window = FCCS::CreateWindowExW(L"fccs", width, height);
-	FCCS::RHI::FCCS_SWAP_CHAIN_DESC desc{};
+	FCCS::FCCS_SWAP_CHAIN_DESC desc{};
 	desc.Width = width;
 	desc.Height = height;
 	desc.Hwnd = window->GetHWND();
