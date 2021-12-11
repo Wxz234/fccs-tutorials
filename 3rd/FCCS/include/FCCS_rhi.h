3705265ad139d8fcd81d1249bc962ccc16a6d407
Wxz <429373960@qq.com>
2021-12-11 13:42:54 +0800

    3rd

--- 3rd/FCCS/include/FCCS_rhi.h
+++ 3rd/FCCS/include/FCCS_rhi.h
@@ -3,14 +3,31 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 namespace FCCS {
-	struct CommandQueue : public FRHIObejct {
 
+	struct Buffer : public FRHIObejct {
+		virtual void Update(void* ptr, uint32 size) = 0;
+		virtual D3D12_VERTEX_BUFFER_VIEW GetVertexBufferView(uint32 stride, uint32 size) = 0;
+		virtual D3D12_INDEX_BUFFER_VIEW GetIndexBufferView(uint32 size, DXGI_FORMAT format) = 0;
+	};
+
+	struct Texture : public FRHIObejct {
+		virtual D3D12_GPU_DESCRIPTOR_HANDLE GetShaderResourceView() = 0;
+	};
+
+	struct CommandList : public FRHIObejct {
+		virtual void Reset(ID3D12PipelineState* pso) = 0;
+		virtual void Close() = 0;
+	};
+	struct CommandQueue : public FRHIObejct {
+		virtual void WaitIdle() = 0;
 	};
 
 	struct RHIDevice : public FRHIObejct {
+		virtual Buffer* CreateBuffer(uint32 size) = 0;
+		virtual Texture* CreateDDSTextureFromFile(const wchar_t* szFileName) = 0;
 		virtual CommandQueue* CreateCommandQueue(D3D12_COMMAND_LIST_TYPE type) = 0;
+		virtual CommandList* CreateCommandList(D3D12_COMMAND_LIST_TYPE type) = 0;
 	};
-	FCCS_API RHIDevice* CreateDevice(uint32 adapter);
 
 	struct FCCS_SWAP_CHAIN_DESC {
 		uint32 BufferCount;
@@ -24,5 +41,11 @@ namespace FCCS {
 	struct SwapChain : public FRHIObejct {
 		virtual void Present(uint32 sync) = 0;
 	};
+
+	struct Blob : public FRHIObejct {
+	};
+
+	FCCS_API RHIDevice* CreateDevice(uint32 adapter);
 	FCCS_API SwapChain* CreateSwapChain(CommandQueue* queue, const FCCS_SWAP_CHAIN_DESC* desc);
+	FCCS_API Blob* CompileShaderFromFile(const wchar_t* pFileName, const char* pEntrypoint, const char* pTarget);
 }
