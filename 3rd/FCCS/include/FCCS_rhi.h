2947c38dbc19e16d5bfe58762a4fdac8f4473e90
Wxz <429373960@qq.com>
2021-12-16 23:19:51 +0800

    update

--- 3rd/FCCS/include/FCCS_rhi.h
+++ 3rd/FCCS/include/FCCS_rhi.h
@@ -23,18 +23,15 @@ namespace FCCS {
 
 	struct Texture : public FRHIObejct {
 		virtual D3D12_GPU_DESCRIPTOR_HANDLE GetShaderResourceView() = 0;
-	};
-
-	struct RenderTarget : public FRHIObejct {
-		virtual void Begin(CommandList *pList) = 0;
-		virtual void End(CommandList* pList) = 0;
+		virtual D3D12_GPU_DESCRIPTOR_HANDLE GetUnorderedAccessView() = 0;
+		virtual D3D12_CPU_DESCRIPTOR_HANDLE GetRenderTargetView() = 0;
+		virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDepthStencilView() = 0;
 	};
 
 	struct RHIDevice : public FRHIObejct {
 		virtual Buffer* CreateBuffer(uint32 size) = 0;
 		virtual Texture* CreateDDSTextureFromFile(const wchar_t* pFileName) = 0;
-		virtual Texture* CreateTexture(const D3D12_RESOURCE_DESC* pDesc, D3D12_RESOURCE_STATES InitialResourceState) = 0;
-		virtual RenderTarget* CreateRenderTarget(const D3D12_RESOURCE_DESC* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue) = 0;
+		virtual Texture* CreateTexture(const D3D12_RESOURCE_DESC* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue = nullptr) = 0;
 		virtual CommandQueue* CreateCommandQueue(D3D12_COMMAND_LIST_TYPE type) = 0;
 		virtual CommandList* CreateCommandList(D3D12_COMMAND_LIST_TYPE type) = 0;
 		virtual RootSignature* CreateRootSignature(const D3D12_ROOT_SIGNATURE_DESC* pDesc) = 0;
