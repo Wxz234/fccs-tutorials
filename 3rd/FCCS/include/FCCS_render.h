955b849a95f202d48c82e677373bb879813ca0a9
Wxz <429373960@qq.com>
2021-12-18 23:09:26 +0800

    1

--- 3rd/FCCS/include/FCCS_render.h
+++ 3rd/FCCS/include/FCCS_render.h
@@ -6,6 +6,10 @@ namespace FCCS {
 	struct Renderer: public FObject {
 		virtual void Begin() = 0;
 		virtual void End() = 0;
+		virtual CommandQueue* GetCommandQueue() const noexcept = 0;
+		virtual CommandList* GetCommandList() const noexcept = 0;
+		virtual RHIDevice* GetDevice() const noexcept = 0;
+		virtual SwapChain* GetSwapChain() const noexcept = 0;
 	};
 
 	FCCS_API Renderer* CreateRenderer(Window *pWindow);
