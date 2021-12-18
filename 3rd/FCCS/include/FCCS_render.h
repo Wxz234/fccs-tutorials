e80228dfd832b1cd1c537e7cacd1bb046c466606
Wxz <429373960@qq.com>
2021-12-18 19:47:29 +0800

    update

--- 3rd/FCCS/include/FCCS_render.h
+++ 3rd/FCCS/include/FCCS_render.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "FCCS_core.h"
 #include "FCCS_rhi.h"
+#include "FCCS_window.h"
 namespace FCCS {
+	struct Renderer: public FObject {
+	};
 
+	FCCS_API Renderer* CreateRenderer(Window *pWindow);
 }
