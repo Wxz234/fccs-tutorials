a57a6d19820080a4cdc8d4c3583a5885e54bdf1a
Wxz <429373960@qq.com>
2021-12-20 22:48:34 +0800

    1

--- 02-InitDevice/InitDevice.cpp
+++ 02-InitDevice/InitDevice.cpp
@@ -2,12 +2,15 @@
 #include <FCCS.h>
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	auto window = FCCS::CreateWindowExW(L"fccs", 800, 600);
-	auto renderer = FCCS::CreateRenderer(window);
+	auto device = FCCS::CreateDevice(0);
+	auto queue = device->CreateCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
+	auto swapchain = FCCS::CreateSwapChain(queue, window->GetHWND(), window->GetWidth(), window->GetHeight());
 	while (window->IsRun()) {
-		renderer->Begin();
-		renderer->End();
+		swapchain->Present(1);
 	}
-	FCCS::DestroyObject(renderer);
 	FCCS::DestroyObject(window);
+	FCCS::DestroyObject(device);
+	FCCS::DestroyObject(queue);
+	FCCS::DestroyObject(swapchain);
 	return 0;
 }
