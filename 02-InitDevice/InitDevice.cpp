4c4c3a3226c534ca509628cc52dd903d1349eed8
Wxz234 <429373960@qq.com>
2021-12-23 16:27:23 +0800

    1

--- 02-InitDevice/InitDevice.cpp
+++ 02-InitDevice/InitDevice.cpp
@@ -1,15 +1,15 @@
 #include <Windows.h>
 #include <FCCS.h>
-
-
+#include <wrl.h>
+#pragma comment(lib,"runtimeobject.lib")
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	Microsoft::WRL::Wrappers::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+	if (FAILED(initialize))
+		return 1;
 	constexpr unsigned width = 800, height = 600;
 	auto window = FCCS::CreateFCCSWindow(L"fccs", width, height);
 	auto device = FCCS::CreateDevice(0);
 	auto swapchain = FCCS::CreateSwapChain(device, window);
-	auto _sc = FCCS::Cast<IDXGISwapChain>(swapchain->GetNativePtr());
-
-
 	while (window->IsRun()) {
 		swapchain->Present(1);
 	}
