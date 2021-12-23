128eb1f21e5dc205c43308ec113a88b838a299fb
Wxz <429373960@qq.com>
2021-12-23 23:34:09 +0800

    1

--- 02-InitDevice/InitDevice.cpp
+++ 02-InitDevice/InitDevice.cpp
@@ -1,7 +1,6 @@
 #include <Windows.h>
 #include <FCCS.h>
 #include <wrl.h>
-#pragma comment(lib,"runtimeobject.lib")
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	constexpr unsigned width = 800, height = 600;
 	auto window = FCCS::CreateFCCSWindow(L"fccs", width, height);
