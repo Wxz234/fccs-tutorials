b966fc44efce06525d3bbfe0ce31375afe4cd239
Wxz <429373960@qq.com>
2022-01-09 20:55:22 +0800

    T1

--- 01-CreateWindow/CreateWindow.cpp
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-#include <FCCS.h>
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-	auto window = FCCS::CreateFCCSWindow(L"fccs", 800, 600);
-	while (window->IsRun()) {}
-	FCCS::DestroyFObject(window);
-	return 0;
-}
