04d17939c43b3ea3df66298a95f68bc365e8b6f9
Wxz <429373960@qq.com>
2022-01-16 23:16:22 +0800

    Create main.cpp

---
+++ 02-InitializeDevice/main.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <fccs/fccs.h>
+
+class Callback : public fccs::window::IWindowCallback {
+public:
+	void Initialize() {}
+	void Update() {}
+	void Release() {}
+};
+
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	auto window = fccs::window::createWindow();
+
+	window->OpenWindow();
+	Callback callback;
+	return window->Run(&callback);
+}
