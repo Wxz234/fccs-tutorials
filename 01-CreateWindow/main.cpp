d27b683238ba9d12a2843785266c16db0ba3830b
Wxz234 <429373960@qq.com>
2022-01-18 15:48:46 +0800

    update t1 t2

--- 01-CreateWindow/main.cpp
+++ 01-CreateWindow/main.cpp
@@ -1,19 +1,26 @@
 #include <Windows.h>
 #include <fccs/fccs.h>
 
-class Callback : public fccs::window::IWindowCallback {
-public:
-	void Initialize() {}
-	void Update() {}
-	void Release() {}
-};
-
-
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	auto window = fccs::window::CreateFCCSWindow();
+	constexpr uint32_t width = 800, height = 600;
+	fccs::window::WindowDesc windowDesc = { width, height, L"fccs" };
+	auto window = fccs::window::CreateFCCSWindow(windowDesc);
+    window->OpenWindow();
+    MSG msg = {};
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            
+        }
+    }
 
-	window->OpenWindow();
-	Callback callback;
-	return window->Run(&callback);
+	fccs::DestroyResource(window);
+	return 0;
 }
--- 02-InitializeDevice/main.cpp
+++ 02-InitializeDevice/main.cpp
@@ -1,34 +1,35 @@
 #include <Windows.h>
 #include <fccs/fccs.h>
 
-class Callback : public fccs::window::IWindowCallback {
-public:
-	Callback(HWND hwnd):hwnd(hwnd) {}
-	void Initialize() {
-		device = fccs::rhi::CreateDeivce();
-		queue = device->CreateCommandQueue(fccs::rhi::CommandQueueType::Graphics);
-		fccs::window::SwapChainDesc swapchainDesc = {};
-		swapchainDesc.width = 800;
-		swapchainDesc.height = 600;
-		swapchainDesc.hwnd = hwnd;
-		swapchain = fccs::window::CreateSwapChain(swapchainDesc, queue.get());
-	}
-	void Update() {
-		swapchain->Present(1);
-	}
-	void Release() {}
-private:
-	HWND hwnd;
-	fccs::rhi::DeviceHandle device;
-	fccs::rhi::CommandQueueHandle queue;
-	fccs::window::SwapChainHandle swapchain;
-};
-
-
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	auto window = fccs::window::CreateFCCSWindow();
-	window->OpenWindow();
-	Callback callback(window->GetHWND());
-	return window->Run(&callback);
+    constexpr uint32_t width = 800, height = 600;
+    fccs::window::WindowDesc windowDesc = { width, height, L"fccs" };
+    auto window = fccs::window::CreateFCCSWindow(windowDesc);
+    window->OpenWindow();
+
+    auto device = fccs::rhi::CreateDeivce();
+    auto queue = device->CreateCommandQueue(fccs::rhi::CommandQueueType::Graphics);
+    fccs::rhi::SwapChainDesc swapchainDesc = { width ,height ,window->GetHWND() };
+    auto swapchain = fccs::rhi::CreateSwapChain(queue, swapchainDesc);
+
+    MSG msg = {};
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            swapchain->Present(1);
+        }
+    }
+
+    fccs::DestroyResource(window);
+    fccs::DestroyResource(device);
+    fccs::DestroyResource(queue);
+    fccs::DestroyResource(swapchain);
+    return 0;
 }
